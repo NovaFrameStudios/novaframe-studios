208483476838011262b11c6ae8e777641848e173
NovaFrameStudios <muhammadkhamis789@gmail.com>
2026-07-05 16:03:20 +0300

    Update reader.js

--- reader.js
+++ reader.js
@@ -31,3 +31,21 @@ updatePage();
 mangaImage.addEventListener("click", () => {
     nextPage();
 });
+let touchStartX = 0;
+let touchEndX = 0;
+
+mangaImage.addEventListener("touchstart", (e) => {
+    touchStartX = e.changedTouches[0].screenX;
+});
+
+mangaImage.addEventListener("touchend", (e) => {
+    touchEndX = e.changedTouches[0].screenX;
+
+    if (touchStartX - touchEndX > 50) {
+        nextPage();
+    }
+
+    if (touchEndX - touchStartX > 50) {
+        previousPage();
+    }
+});
